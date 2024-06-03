6b82ee9de6d1f686b6fcdf8f98975f0bcfc73d72
ojiwon <ojw970725@gmail.com>
2024-06-03 22:01:52 +0900

    feat : 0~9 사이 랜덤번호 생성 기능 구현

---
+++ src/main/java/RandomNumberGenerator.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class RandomNumberGenerator {
+
+    public static int generate() {
+        Random random = new Random();
+        return random.nextInt(10);
+    }
+}
---
+++ src/test/java/RandomNumberGenratorTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class RandomNumberGenratorTest {
+
+    @Test
+    @DisplayName("0~9사이 랜덤번호 생성")
+    public void generate() {
+        assertThat(RandomNumberGenerator.generate()).isBetween(0,9);
+    }
+}
