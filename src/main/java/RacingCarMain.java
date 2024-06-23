98dfb6ce6be6f35eaff9197df2913b9bacebf5ff
ojiwon <ojw970725@gmail.com>
2024-06-23 14:40:45 +0900

    feat : main method 구현

---
+++ src/main/java/RacingCarMain.java
@@ -0,0 +1,20 @@
+import domain.Cars;
+
+public class RacingCarMain {
+
+    public static void main(String[] args) {
+        Cars cars = InputView.inputCarName();
+        int count = InputView.inputGameCount();
+
+        System.out.println("실행 결과");
+        System.out.println(cars.toString());
+
+        for(int i = 0; i < count; i++){
+            cars.move();
+            System.out.println(cars.toString());
+        }
+
+        String winners = cars.getWinner();
+        System.out.println(winners +"가 최종 우승했습니다.");
+    }
+}
